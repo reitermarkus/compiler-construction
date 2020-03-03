e5d7b43ae0a2e6078fb5a12d7826e681fa9f63cd
Markus Reiter <me@reitermark.us>
2020-03-03 07:44:19 +0100

    Add grammar for `int` and `float`.

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,13 +1,32 @@
-pub fn parse() -> i64 {
-  2 + 2
-}
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct McParser;
 
 #[cfg(test)]
 mod tests {
+  use pest::{parses_to, consumes_to};
+
   use super::*;
 
   #[test]
-  fn it_works() {
-    assert_eq!(parse(), 4);
+  fn parse_int() {
+    parses_to! {
+      parser: McParser,
+      input:  "42",
+      rule:   Rule::int,
+      tokens: [int(0, 2)]
+    };
+  }
+
+  #[test]
+  fn parse_float() {
+    parses_to! {
+      parser: McParser,
+      input:  "4.2",
+      rule:   Rule::float,
+      tokens: [float(0, 3)]
+    };
   }
 }
