c2cc1578378a158e448921177618d45c74e26155
Markus Reiter <me@reitermark.us>
2020-03-11 16:40:51 +0100

    Add `mc_symbol_table` binary.

---
+++ mc_symbol_table/src/main.rs
@@ -0,0 +1,16 @@
+#![deny(missing_debug_implementations, rust_2018_idioms)]
+
+use clap::{value_t, App, Arg};
+
+fn main() {
+  let matches = App::new("mC Symbol Table Viewer")
+    .set_term_width(0)
+    .max_term_width(0)
+    .about("Utility for viewing the generated intermediate representation. Errors are reported on invalid inputs.")
+    .arg(Arg::from_usage("-o, --output <out-file> 'output file (defaults to stdout)'").required(false))
+    .arg(Arg::from_usage("<file> 'input file (use '-' to read from stdin)'"))
+    .get_matches();
+
+  let out_file = value_t!(matches, "output", String).ok();
+  let in_file = value_t!(matches, "file", String).unwrap();
+}
