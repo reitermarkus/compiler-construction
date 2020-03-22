e7b9153eb6fc61d36e72c17272f338a368aa5c47
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-03-22 23:22:08 +0100

    Reduce size of program at test `format_symbol_table`.

--- mc_symbol_table/src/format_symbol_table.rs
+++ mc_symbol_table/src/format_symbol_table.rs
@@ -1,7 +1,7 @@
-use prettytable::{format, Cell, Row, Table};
 use format::consts::FORMAT_NO_LINESEP_WITH_TITLE;
+use prettytable::{format, Cell, Row, Table};
 
-use crate::symbol_table::*;
+use crate::symbol_table::{Scope, ScopeTable, SymbolTable};
 
 impl ScopeTable {
   pub fn to_pretty_tables(&self) -> Vec<Table> {
@@ -28,7 +28,6 @@ impl SymbolTable {
 mod tests {
   use crate::to_symbol_table::*;
   use mc_parser::ast::*;
-  use pest::Span;
 
   use super::*;
 
@@ -43,35 +42,11 @@ mod tests {
           Parameter { ty: Ty::Bool, count: None, identifier: Identifier::from("debug") },
         ],
         body: CompoundStatement {
-          statements: vec![
-            Statement::If(Box::new(IfStatement {
-              condition: Expression::Binary {
-                op: BinaryOp::Lt,
-                lhs: Box::new(Expression::Variable {
-                  identifier: Identifier::from("n"),
-                  index_expression: None,
-                  span: Span::new("", 0, 0).unwrap(),
-                }),
-                rhs: Box::new(Expression::Literal { literal: Literal::Int(2), span: Span::new("", 0, 0).unwrap() }),
-                span: Span::new("", 0, 0).unwrap(),
-              },
-              block: Statement::Compound(CompoundStatement {
-                statements: vec![Statement::Decl(Declaration {
-                  identifier: Identifier::from("x"),
-                  count: None,
-                  ty: Ty::Float,
-                })],
-              }),
-              else_block: Some(Statement::Compound(CompoundStatement {
-                statements: vec![Statement::Decl(Declaration {
-                  identifier: Identifier::from("x"),
-                  count: None,
-                  ty: Ty::Int,
-                })],
-              })),
-            })),
-            Statement::Decl(Declaration { identifier: Identifier::from("y"), count: None, ty: Ty::String }),
-          ],
+          statements: vec![Statement::Decl(Declaration {
+            identifier: Identifier::from("y"),
+            count: None,
+            ty: Ty::String,
+          })],
         },
       }],
     };
