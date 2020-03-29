d4366bfff0f87e6efbeed1dd6584ee7c76f5ccdd
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-03-29 18:53:23 +0200

    Add test for semantic `variable expression` checks.

--- mc_symbol_table/src/semantic_error.rs
+++ mc_symbol_table/src/semantic_error.rs
@@ -8,6 +8,7 @@ use mc_parser::ast::*;
 
 use crate::*;
 
+#[derive(PartialEq, Debug)]
 pub enum SemanticError<'a> {
   #[allow(dead_code)]
   Type {
@@ -50,16 +51,12 @@ impl fmt::Display for SemanticError<'_> {
       Self::Type { span, expected, actual } => write_err!(f, span, "expected type {}, found type {}", expected, actual),
       Self::NotDeclared { span, identifier } => {
         write_err!(f, span, "variable '{}' not declared in this scope", identifier)
-      },
-      Self::IndexError { span, identifier } => {
-        write_err!(f, span, "wrong index expression for '{}'", identifier)
-      },
+      }
+      Self::IndexError { span, identifier } => write_err!(f, span, "wrong index expression for '{}'", identifier),
       Self::IndexOutOfBound { span, identifier, size, actual } => {
         write_err!(f, span, "index {} out of bound for '{}' with size {}", actual, identifier, size)
-      },
-      Self::WrongUseOfFunction { span, identifier } => {
-        write_err!(f, span, "wrong use of function '{}'", identifier)
-      },
+      }
+      Self::WrongUseOfFunction { span, identifier } => write_err!(f, span, "wrong use of function '{}'", identifier),
     }
   }
 }
@@ -78,13 +75,17 @@ impl CheckSemantics for Expression<'_> {
         if let Some(var) = Scope::lookup(scope, identifier) {
           if let Symbol::Function(..) = var {
             errors.push(SemanticError::WrongUseOfFunction { span: span.clone(), identifier: identifier.clone() })
-          }
-
-          if let Some(index) = index_expression {
-            match index.check_index_semantics(&var, identifier).err() {
-              Some(Some(e)) => { errors.push(e); },
-              Some(None) => { errors.push(SemanticError::IndexError { span: span.clone(), identifier: identifier.clone()}) }
-              None => {}
+          } else {
+            if let Some(index) = index_expression {
+              match index.check_index_semantics(&var, identifier).err() {
+                Some(Some(e)) => {
+                  errors.push(e);
+                }
+                Some(None) => {
+                  errors.push(SemanticError::IndexError { span: span.clone(), identifier: identifier.clone() })
+                }
+                None => {}
+              }
             }
           }
         } else {
@@ -108,27 +109,89 @@ trait CheckIndexSemantics {
 
 impl CheckIndexSemantics for Expression<'_> {
   fn check_index_semantics(&self, symbol: &Symbol, identifier: &Identifier) -> Result<(), Option<SemanticError<'_>>> {
-    if let Symbol::Variable( .., Some(size) ) = symbol {
-
+    if let Symbol::Variable(.., Some(size)) = symbol {
       match self {
         Self::Literal { literal, span } => {
-
           if let Literal::Int(index) = literal {
             if *index < *size as i64 {
               Ok(())
             } else {
-              Err(Some(SemanticError::IndexOutOfBound { span: span.clone(), identifier: identifier.clone(), size: size.clone(), actual: index.clone() as usize }))
+              Err(Some(SemanticError::IndexOutOfBound {
+                span: span.clone(),
+                identifier: identifier.clone(),
+                size: size.clone(),
+                actual: index.clone() as usize,
+              }))
             }
           } else {
-            Err(Some(SemanticError::IndexError { span: span.clone(), identifier: identifier.clone()}))
+            Err(Some(SemanticError::IndexError { span: span.clone(), identifier: identifier.clone() }))
           }
-        },
-        _unreachable => {
-          Err(None)
         }
+        _unreachable => Err(None),
       }
     } else {
       Err(None)
     }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use pest::Span;
+
+  use super::*;
+
+  #[test]
+  fn semantic_expression_variable_check() {
+    let variable_without_index = Expression::Variable {
+      identifier: Identifier::from("x"),
+      span: Span::new("x", 0, 1).unwrap(),
+      index_expression: None,
+    };
+
+    let variable_with_index = Expression::Variable {
+      identifier: Identifier::from("x"),
+      span: Span::new("x[10]", 0, 5).unwrap(),
+      index_expression: Some(Box::new(Expression::Literal {
+        span: Span::new("x[10]", 2, 4).unwrap(),
+        literal: Literal::Int(10),
+      })),
+    };
+
+    let mut scope = Scope::new();
+
+    let mut result = variable_without_index.check_semantics(&scope);
+    let mut errors = result.expect_err("no errors found");
+    assert!(errors.contains(&SemanticError::NotDeclared {
+      span: Span::new("x", 0, 1).unwrap(),
+      identifier: Identifier::from("x")
+    }));
+    assert_eq!(errors.len(), 1);
+
+    scope.borrow_mut().symbols.insert(Identifier::from("x"), Symbol::Variable(Ty::Int, None));
+    result = variable_without_index.check_semantics(&scope);
+    assert_eq!(result, Ok(()));
+
+    scope = Scope::new();
+    scope.borrow_mut().symbols.insert(Identifier::from("x"), Symbol::Variable(Ty::Int, Some(5)));
+    result = variable_with_index.check_semantics(&scope);
+    errors = result.expect_err("no errors found");
+    assert!(errors.contains(&SemanticError::IndexOutOfBound {
+      span: Span::new("x[10]", 2, 4).unwrap(),
+      identifier: Identifier::from("x"),
+      actual: 10,
+      size: 5
+    }));
+    assert_eq!(errors.len(), 1);
+
+    scope = Scope::new();
+    scope.borrow_mut().symbols.insert(Identifier::from("x"), Symbol::Function(Some(Ty::Int)));
+    result = variable_with_index.check_semantics(&scope);
+    errors = result.expect_err("no errors found");
+    assert!(errors.contains(&SemanticError::WrongUseOfFunction {
+      span: Span::new("x[10]", 0, 5).unwrap(),
+      identifier: Identifier::from("x")
+    }));
+    assert_eq!(errors.len(), 1);
   }
 }
