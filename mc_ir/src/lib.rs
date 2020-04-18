0217d4719138aae416658a11156db104dd630e89
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-04-18 17:21:21 +0200

    Add `mc_ir`.

---
+++ mc_ir/src/lib.rs
@@ -0,0 +1,19 @@
+#![deny(missing_debug_implementations, rust_2018_idioms)]
+
+use std::fs::File;
+use std::io::{prelude::*, stdin};
+use std::path::Path;
+
+pub fn mc_ir(in_file: impl AsRef<Path>, mut _out_stream: impl Write) -> std::io::Result<()> {
+  let mut contents = String::new();
+
+  if in_file.as_ref() == Path::new("-") {
+    stdin().read_to_string(&mut contents)?;
+  } else {
+    File::open(in_file)?.read_to_string(&mut contents)?;
+  }
+
+  let _ast = mc_parser::parse(&contents).expect("failed to parse program");
+
+  Ok(())
+}
