1b1f03151cf51564c101c8051e994d0146cfafcc
Markus Reiter <me@reitermark.us>
2020-06-10 00:31:46 +0200

    Support Docker backend.

--- mcc/src/lib.rs
+++ mcc/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_debug_implementations, rust_2018_idioms)]
 
-use std::ffi::OsString;
+use std::env::current_dir;
 use std::fs::File;
 use std::io::{prelude::*, stdin};
 use std::path::Path;
@@ -11,18 +11,14 @@ use mc_asm::ToAsm;
 use mc_ir::AddToIr;
 use mc_ir::IntermediateRepresentation;
 
-use tempfile::tempdir;
-
 pub fn mcc(in_file: impl AsRef<Path>, out_file: impl AsRef<Path>, backend: String, quiet: bool) -> std::io::Result<()> {
   let mut contents = String::new();
 
-  let asm_file_name = if in_file.as_ref() == Path::new("-") {
+  if in_file.as_ref() == Path::new("-") {
     stdin().read_to_string(&mut contents)?;
-    OsString::from("a.s")
   } else {
     let in_file = in_file.as_ref().canonicalize()?;
     File::open(&in_file)?.read_to_string(&mut contents)?;
-    in_file.with_extension("s").file_name().unwrap().to_os_string()
   };
 
   let ast = mc_parser::parse(&contents).expect("failed to parse program");
@@ -34,29 +30,43 @@ pub fn mcc(in_file: impl AsRef<Path>, out_file: impl AsRef<Path>, backend: Strin
 
   let asm = ir.to_asm();
 
-  let temp_dir = tempdir()?;
-
-  let asm_file = temp_dir.path().join(asm_file_name);
-  let mut asm_stream = File::create(&asm_file)?;
-  write!(&mut asm_stream, "{}", asm)?;
-
   let stderr = if quiet {
     Stdio::null()
   } else {
     Stdio::inherit()
   };
 
-  let mut command = Command::new(backend)
-    .arg("-m32")
-    .arg(asm_file)
-    .arg("-o")
-    .arg(out_file.as_ref())
-    .stderr(stderr)
-    .spawn()?;
+  let mut command = Command::new(&backend);
 
-  let status = command.wait()?;
+  if backend == "docker" {
+    command.arg("run");
+    command.arg("-i");
 
-  temp_dir.close()?;
+    command.arg("-v");
+    command.arg(format!("{}:/root", current_dir().unwrap().display()));
+
+    command.arg("gcc");
+    command.arg("gcc");
+  }
+
+  command.arg("-m32");
+  command.arg("-o");
+  command.arg(out_file.as_ref());
+  command.arg("-x");
+  command.arg("assembler");
+  command.arg("-");
+
+  command.stdin(Stdio::piped());
+  command.stderr(stderr);
+
+  let mut child = command.spawn()?;
+
+  let mut stdin = child.stdin.take().unwrap();
+  write!(&mut stdin, "{}", asm)?;
+
+  drop(stdin);
+
+  let status = child.wait()?;
 
   if !status.success() {
     match status.code() {
