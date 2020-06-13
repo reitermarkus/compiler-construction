fdef2931de1348b2ceeaf565b9ae9acf7c8653f8
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-06-13 17:27:30 +0200

    Generate cfg blocks for labels.

--- mc_cfg_to_dot/src/cfg.rs
+++ mc_cfg_to_dot/src/cfg.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
-use mc_parser::ast::Identifier;
 use mc_ir::*;
+use mc_parser::ast::Identifier;
 
 #[derive(Debug, Default)]
 pub struct Cfg {
@@ -11,6 +11,20 @@ pub struct Cfg {
 
 pub type CfgBlock = Vec<String>;
 
+impl Cfg {
+  fn push_line(&mut self, line: String) {
+    if let Some(block) = self.blocks.last_mut() {
+      block.push(line);
+    } else {
+      self.blocks.push(vec![line]);
+    }
+  }
+
+  fn push_block(&mut self) {
+    self.blocks.push(Vec::new());
+  }
+}
+
 pub trait ToCfg {
   fn to_cfg(&self) -> Cfg;
 }
@@ -19,22 +33,23 @@ impl ToCfg for IntermediateRepresentation<'_> {
   fn to_cfg(&self) -> Cfg {
     let mut cfg = Cfg::default();
 
-    let labels = self.statements.iter().filter_map(|statement| match statement {
-        Op::Jumpfalse(_, Arg::Reference(_, reference)) | Op::Jump(Arg::Reference(_, reference)) => {
-          Some(reference)
-        }
-        _ => None
-      }).collect::<Vec<_>>();
+    let labels = self
+      .statements
+      .iter()
+      .filter_map(|statement| match statement {
+        Op::Jumpfalse(_, Arg::Reference(_, reference)) | Op::Jump(Arg::Reference(_, reference)) => Some(reference),
+        _ => None,
+      })
+      .collect::<Vec<_>>();
 
     let (main_range, _) = self.functions.get(&Identifier::from("main")).expect("no main function found");
 
-    let mut block = CfgBlock::new();
-
     for i in main_range.clone() {
-      block.push(self.statements[i].to_string());
+      if labels.contains(&&i) {
+        cfg.push_block();
+      }
+      cfg.push_line(self.statements[i].to_string());
     }
-
-    cfg.blocks.push(block);
 
     cfg
   }
