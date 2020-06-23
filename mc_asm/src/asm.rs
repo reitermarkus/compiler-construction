568a7c7c20b0060056d0061f56dc4c7265a09dde
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-06-23 22:54:13 +0200

    Add documentation for `asm`.

--- mc_asm/src/asm.rs
+++ mc_asm/src/asm.rs
@@ -7,6 +7,12 @@ use mc_parser::ast::*;
 
 use crate::storage::*;
 
+/// Returns a string formatted as a label.
+///
+/// # Arguments
+///
+/// * `label` - A string to be formatted as a label.
+///
 #[macro_export]
 macro_rules! l {
   { $label:expr } => (
@@ -14,6 +20,21 @@ macro_rules! l {
   );
 }
 
+/// Returns a string formatted as an assembly instruction.
+///
+/// # Arguments
+///
+/// * `instruction` - A string representing an instruction.
+///
+/// * `instruction` - A string representing an instruction.
+/// * `arg` - An Argument used with the instruction
+///
+/// * `lhs` - An Argument used with the `movzx` instruction.
+/// * `rhs` - An Argument used with the `movzx` instruction.
+///
+/// * `instruction` - A string representing an instruction.
+/// * `lhs` - An Argument used with the `movzx` instruction.
+/// * `rhs` - An Argument used with the `movzx` instruction.
 #[macro_export]
 macro_rules! i {
   { $instruction:expr } => (
@@ -30,12 +51,18 @@ macro_rules! i {
   );
 }
 
+/// `Asm` repreents a global struct for all components of the assembly output.
 #[derive(Debug)]
 pub struct Asm {
+  /// All assembly lines, that will be in the output of the assembly file.
   pub lines: Vec<String>,
+  /// All *labels*, that will be in the output of the assembly file.
   pub labels: BTreeMap<usize, String>,
+  /// All *strings*, that will be in the output of the assembly file.
   pub strings: BTreeMap<String, String>,
+  /// All *floats*, that will be in the output of the assembly file.
   pub floats: BTreeMap<OrderedFloat<f64>, String>,
+    /// All built-in functions, that will be in the output of the assembly file like `read_int`, `read_float` or `print_nl`.
   pub builtin_functions: HashSet<Identifier>,
 }
 
@@ -61,6 +88,12 @@ impl fmt::Display for Asm {
 }
 
 impl Asm {
+  /// Returns a [`Label`](crate::storage::Storage::Label) containing a [`Dword`](crate::storage::StorageType::Dword), the actual label and a boolean, that specifies that *OFFSET FLAT* is not required in the output.
+  ///
+  /// # Arguments
+  ///
+  /// * `float` - The floating point number to be repsented in a label.
+  ///
   pub fn add_float(&mut self, float: f64) -> Storage {
     let float = OrderedFloat::from(float);
 
@@ -69,12 +102,24 @@ impl Asm {
     Storage::Label(StorageType::Dword, label.to_owned(), false)
   }
 
+  /// Returns a [`Label`](storage::Storage::Label) containing a [`Dword`](storage::StorageType::Dword), the actual label and a boolean, that specifies that *OFFSET FLAT* is required in the output.
+  ///
+  /// # Arguments
+  ///
+  /// * `s` - The string to be repsented in a label.
+  ///
   pub fn add_string(&mut self, s: &str) -> Storage {
     let string_number = self.strings.len();
     let label = self.strings.entry(s.to_owned()).or_insert_with(|| format!(".LS{}", string_number));
     Storage::Label(StorageType::Dword, label.to_owned(), true)
   }
 
+  /// Loads a float.
+  ///
+  /// # Arguments
+  ///
+  /// * `storage` - The [`Storage`](storage::Storage) enum representing the float to be loaded.
+  ///
   pub fn load_float(&mut self, storage: &Storage) {
     if matches!(storage, Storage::Fpu(..)) {
       self.lines.push(i! { "nop" });
