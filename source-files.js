d7f2eb28630e93d0036afc440b18acd8a42b3214
reitermarkus <me@reitermark.us>
2020-06-23 16:55:03 +0000

    Deploying to gh-pages from  @ fa3fe13f914efd3c168b66246ae4f2dfbbe74b3d 🚀

--- source-files.js
+++ source-files.js
@@ -5,6 +5,6 @@ sourcesIndex["mc_cfg_to_dot"] = {"name":"","files":["cfg.rs","lib.rs"]};
 sourcesIndex["mc_error"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["mc_ir"] = {"name":"","files":["add_to_ir.rs","format_ir.rs","ir.rs","lib.rs"]};
 sourcesIndex["mc_parser"] = {"name":"","files":["ast.rs","lib.rs"]};
-sourcesIndex["mc_symbol_table"] = {"name":"","files":["add_to_scope.rs","format_symbol_table.rs","lib.rs","semantic_checks.rs","semantic_error.rs","symbol_table.rs"]};
+sourcesIndex["mc_symbol_table"] = {"name":"","files":["add_to_scope.rs","cli.rs","format_symbol_table.rs","lib.rs","semantic_checks.rs","semantic_error.rs","symbol_table.rs"]};
 sourcesIndex["mcc"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
